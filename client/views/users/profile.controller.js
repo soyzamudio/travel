12219c4b5348be9050147a6a8399ed2c217c4890
Jose Zamudio <zamudio@outlook.com>
2015-03-12 23:20:02 -0700

    Add email input to profile form.

--- client/views/users/profile.controller.js
+++ client/views/users/profile.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('angular-prototype')
-  .controller('ProfileCtrl', ['$rootScope', '$scope', '$state', 'User', function($rootScope, $scope, $state, User){
+  .controller('ProfileCtrl', ['$rootScope', '$scope', '$state', '$window' ,'User', function($rootScope, $scope, $state, $window, User){
     $scope.edit = function(user) {
       User.update(user)
       .then(response => {
+        $window.localStorage.user = JSON.stringify(response.data.user);
         $rootScope.user = response.data.user;
         $state.go('home');
       });
--- server/routes/users/profile.js
+++ server/routes/users/profile.js
@@ -6,7 +6,8 @@ module.exports = {
   handler: function(request, reply) {
     User.findById(request.auth.credentials._id, (err, user) => {
       user.phone = request.payload.phone;
-      user.update(err => {
+      user.email = request.payload.email;
+      user.save(err => {
         if (err) { reply().code(400); }
         reply({user:user}).code(200);
       });
